ca216ce48ccbf777c6fd65ed9a7dfd3d139b9bca
seunggyu.jung <seunggyujung008@gmail.com>
2023-08-28 23:09:21 +0900

    20230828 수업 내용 업뎃

---
+++ 20230828/developer/js/svg.js
@@ -0,0 +1,50 @@
+// dom에서 path 찾기
+const path = document.querySelector('#path');
+const openBtn = document.querySelector('.btn-open');
+
+// path의 전체 길이 구하기
+const pathLength = path.getTotalLength();
+console.log(pathLength) // 2507.61328125
+
+
+// dash 의 길이와 공백
+// path.style.strokeDasharray = 1000 + " " + 1000;
+// path.style.strokeDasharray = pathLength + " " + pathLength;
+path.style.strokeDasharray = `${pathLength} ${pathLength}`;
+
+// dash를 어디서 부터 그릴지
+path.style.strokeDashoffset = pathLength;
+
+console.log('전체 path 길이:', pathLength)
+
+
+function scrollHandler() {
+    // 현재 스크롤의 위치
+    const scrollTop = document.documentElement.scrollTop;
+
+    // 전체 스크롤의 길이
+    const scrollHeight = document.documentElement.scrollHeight;
+
+    // 뷰포트의 높이
+    const clientHeight = document.documentElement.clientHeight;
+
+    // 스크롤의 위치 => % 0 - 1 의 값을 가짐
+    const scrollPercentage = scrollTop / (scrollHeight - clientHeight)
+
+    const drawLength = pathLength * scrollPercentage;
+
+    console.log(drawLength)
+
+    //pathLength -> 0
+    //내리다가 안나타나게 됨.
+    path.style.strokeDashoffset = pathLength - drawLength;
+
+    openBtn.style.opacity = scrollPercentage;
+
+    if(scrollPercentage > 0.8) {
+        openBtn.disabled = false;
+    } else {
+        openBtn.disabled = true;
+    }
+}
+window.addEventListener('scroll', scrollHandler)
